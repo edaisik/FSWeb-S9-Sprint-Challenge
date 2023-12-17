47ce100e2b4e8dc106ecfc77dcfe620167d0204f
edaisik <eda.isik95@hotmail.com>
2023-12-17 21:16:27 +0300

    proje tamamlandı

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,16 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import { findByText, render, screen } from "@testing-library/react";
+import AppFunctional from "./AppFunctional";
+import { userEvent } from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+
+beforeEach(() => {
+  render(<AppFunctional />);
+});
+it("uptest", async () => {
+  const up = screen.getByTestId("up");
+  userEvent.click(up);
+  const message = await screen.findByText(/2, 1/);
+  expect(message).toBeInTheDocument();
+});
